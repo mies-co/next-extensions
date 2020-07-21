637d7b1d8452c9e9ff84f56eac2edc4a75828dc0
Sébastien d'Olbekce <sebastien@mies.co>
2020-07-21 10:28:26 +0200

    Format next-ant-example

--- examples/next-ant-example/src/pages/index.js
+++ examples/next-ant-example/src/pages/index.js
@@ -3,18 +3,11 @@ import { Row, Col, Button, Typography } from "antd";
 const { Title, Text } = Typography;
 
 export default () => (
-  <Row
-    align="middle"
-    justify="center"
-    style={{ height: "100vh", flexDirection: "column" }}
-  >
-    <Title>Next-Ant</Title>
-    <Text style={{ color: theme.primaryColorDark }}>
-      A theme object is globally available. The scss variables are inside it, in
-      camelCase.
-    </Text>
-    <Text>If the button is pink, it means it's working.</Text>
-    <br />
-    <Button type="primary">Hello</Button>
-  </Row>
+	<Row align="middle" justify="center" style={{ height: "100vh", flexDirection: "column" }}>
+		<Title>Next-Ant</Title>
+		<Text style={{ color: theme.primaryColorDark }}>A theme object is globally available. The scss variables are inside it, in camelCase.</Text>
+		<Text>If the button is pink, it means it's working.</Text>
+		<br />
+		<Button type="primary">Hello</Button>
+	</Row>
 );
