777e797e452d99f1f4e6f2742b5a571875fd98ce
Sébastien d'Olbekce <sebastien@mies.co>
2020-07-30 20:43:51 +0200

    Slightly modified configs

--- examples/next-lng-example/next.config.js
+++ examples/next-lng-example/next.config.js
@@ -1,15 +1,21 @@
 module.exports = {
+	basePath: "/v0",
 	publicRuntimeConfig: {
 		lngConfig: {
 			languages: ["en", "fr"],
-			cookie: {
-				name: "next-lng",
-				maxAge: 30 * 24 * 60 * 60,
-			},
-			path: "public/static/translations",
-			options: {
-				shallow: true,
-			},
+			//
+			// Uncomment to override
+			//
+			// path: "public/static/translations",
+			// options: {
+			// 	apiUri: "/api/lng",
+			// 	shallow: true,
+			// 	cookie: {
+			// 		name: "next-lng",
+			// 		path: "/",
+			// 		maxAge: 30 * 24 * 60 * 60,
+			// 	},
+			// },
 		},
 	},
 };
